4644c249339e85e465a27ef2621120f97e935baf
EricSanabria <eric.sanabria12@gmail.com>
2022-07-14 18:33:23 -0300

    first commit

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ buttonList.addEventListener('click', ()=>{
 }
 )
 
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+   $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+   $('video', this).get(0).pause(); 
+}
+
